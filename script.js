202cb0420f62401f5a642f494c7a3135d5398208
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-06-18 21:22:34 +0100

    error404

--- script.js
+++ script.js
@@ -55,54 +55,54 @@ function togglePsw() {
   
 
   function togglePS() {
-    var a = document.getElementById("Cpassword");
-    var b = document.getElementById("hide3");
-    var c = document.getElementById("hide4");
-  
-    if (a.type === "password") {
-      a.type = "text";
-      b.style.display = "none";
-      c.style.display = "block";
-    } else {
-      a.type = "password";
-      b.style.display = "block";
-      c.style.display = "none";
-    }
+  var a = document.getElementById("Cpassword");
+  var b = document.getElementById("hide3");
+  var c = document.getElementById("hide4");
+
+  if (a.type === "password") {
+    a.type = "text";
+    b.style.display = "none";
+    c.style.display = "block";
+  } else {
+    a.type = "password";
+    b.style.display = "block";
+    c.style.display = "none";
   }
+}
   var e = document.getElementById("myInput");
   var f = document.getElementById("myInput2");
   
   /* if(e === f){
       
   } */
-//   let form;
-//   form = document.querySelector("form");
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     checkValidation();
-//   });
-//   let checkValidity = () => {
-//     let [validpassword, validCpassword] = [false, false];
-//     if (password.value == "") {
-//       error.style.display = "block";
-//     } else if (Cpassword.value == "") {
-//       /* let errohtml =  */
-//       /* errohtml */
-//       /* console.log(error.innerHTML); */
-//       error.style.display = "block";
-//       error.innerHTML = "Confirm Password can't be empty!";
-//     } else if (password.value != Cpassword.value) {
-//       error.style.display = "block";
-//       error.innerHTML = "Password does not match";
-//     } else {
-//       error.style.display = "none";
-//       validCpassword = true;
-//       validpassword = true;
-//     }
+  let form;
+  form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkValidation();
+  });
+  let checkValidity = () => {
+    let [validpassword, validCpassword] = [false, false];
+    if (password.value == "") {
+      error.style.display = "block";
+    } else if (Cpassword.value == "") {
+      /* let errohtml =  */
+      /* errohtml */
+      /* console.log(error.innerHTML); */
+      error.style.display = "block";
+      error.innerHTML = "Confirm Password can't be empty!";
+    } else if (password.value != Cpassword.value) {
+      error.style.display = "block";
+      error.innerHTML = "Password does not match";
+    } else {
+      error.style.display = "none";
+      validCpassword = true;
+      validpassword = true;
+    }
   
-//     return validpassword && validCpassword;
-//   };
-//   function checkValidation() {
-//     let nextPage = "./pcs.html";
-//     checkValidity() ? window.location.assign(nextPage) : " ";
-//   }
+    return validpassword && validCpassword;
+  };
+  function checkValidation() {
+    let nextPage = "./pcs.html";
+    checkValidity() ? window.location.assign(nextPage) : " ";
+  }
