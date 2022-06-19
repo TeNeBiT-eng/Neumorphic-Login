4d0cdf5bfd48be679a51881ccf4096614420b63c
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-06-19 13:45:55 +0100

    ...

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-<h1>A NEUMORPHIC LOGIN FORM
+<h1>A NEUMORPHIC LOGIN FORM</h1>
+<p>https://ghap-neumorphic-login.netlify.app/</p>
--- script.js
+++ script.js
@@ -7,55 +7,53 @@ let aContainer = document.querySelector("#a-container");
 let bContainer = document.querySelector("#b-container");
 let allButtons = document.querySelectorAll(".submit");
 
-let getButtons = (e) => e.preventDefault()
+let getButtons = (e) => e.preventDefault();
 
 let changeForm = (e) => {
+  switchCtn.classList.add("is-gx");
+  setTimeout(function () {
+    switchCtn.classList.remove("is-gx");
+  }, 1500);
 
-    switchCtn.classList.add("is-gx");
-    setTimeout(function(){
-        switchCtn.classList.remove("is-gx");
-    }, 1500)
+  switchCtn.classList.toggle("is-txr");
+  switchCircle[0].classList.toggle("is-txr");
+  switchCircle[1].classList.toggle("is-txr");
 
-    switchCtn.classList.toggle("is-txr");
-    switchCircle[0].classList.toggle("is-txr");
-    switchCircle[1].classList.toggle("is-txr");
-
-    switchC1.classList.toggle("is-hidden");
-    switchC2.classList.toggle("is-hidden");
-    aContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-z200");
-}
+  switchC1.classList.toggle("is-hidden");
+  switchC2.classList.toggle("is-hidden");
+  aContainer.classList.toggle("is-txl");
+  bContainer.classList.toggle("is-txl");
+  bContainer.classList.toggle("is-z200");
+};
 
 let mainF = (e) => {
-    for (var i = 0; i < allButtons.length; i++)
-        allButtons[i].addEventListener("click", getButtons );
-    for (var i = 0; i < switchBtn.length; i++)
-        switchBtn[i].addEventListener("click", changeForm)
-}
+  for (var i = 0; i < allButtons.length; i++)
+    allButtons[i].addEventListener("click", getButtons);
+  for (var i = 0; i < switchBtn.length; i++)
+    switchBtn[i].addEventListener("click", changeForm);
+};
 
 window.addEventListener("load", mainF);
 
-
 function togglePsw() {
-    var x = document.getElementById("password");
-    var y = document.getElementById("hide1");
-    var z = document.getElementById("hide2");
-  
-    if (x.type === "password") {
-      x.type = "text";
-      y.style.display = "none";
-      z.style.display = "block";
-    } else {
-      x.type = "password";
-      y.style.display = "block";
-      z.style.display = "none";
-    }
+  let x = document.getElementById("password");
+  let y = document.getElementById("hide1");
+  let z = document.getElementById("hide2");
+
+  if (x.type === "password") {
+    x.type = "text";
+    y.style.display = "none";
+    z.style.display = "block";
+  } else {
+    x.type = "password";
+    y.style.display = "block";
+    z.style.display = "none";
   }
-  function togglePS() {
-  var a = document.getElementById("Cpassword");
-  var b = document.getElementById("hide3");
-  var c = document.getElementById("hide4");
+}
+function togglePS() {
+  let a = document.getElementById("Cpassword");
+  let b = document.getElementById("hide3");
+  let c = document.getElementById("hide4");
 
   if (a.type === "password") {
     a.type = "text";
@@ -67,47 +65,3 @@ function togglePsw() {
     c.style.display = "none";
   }
 }
-// function toggleBtn() {
-//   if (password == "") {
-//         error.style.display = "block";
-//       } else if (Cpassword == "") {
-//         error.style.display = "block";
-//         error.innerHTML = "Confirm Password can't be empty!";
-//       } else if (password != Cpassword) {
-//         error.style.display = "block";
-//         error.innerHTML = "Password does not match";
-//       } else {
-//         error.style.display = "none";
-//         validCpassword = true;
-//         validpassword = true;
-//       }
-// }
-
-  // let form;
-  // form = document.querySelector("form");
-  // form.addEventListener("submit", (e) => {
-  //   e.preventDefault();
-  //   checkValidation();
-  // });
-  // let checkValidity = () => {
-  //   let [validpassword, validCpassword] = [false, false];
-  //   if (password.value == "") {
-  //     error.style.display = "block";
-  //   } else if (Cpassword.value == "") {
-  //     error.style.display = "block";
-  //     error.innerHTML = "Confirm Password can't be empty!";
-  //   } else if (password.value != Cpassword.value) {
-  //     error.style.display = "block";
-  //     error.innerHTML = "Password does not match";
-  //   } else {
-  //     error.style.display = "none";
-  //     validCpassword = true;
-  //     validpassword = true;
-  //   }
-  
-  //   return validpassword && validCpassword;
-  // };
-  // function checkValidation() {
-  //   let nextPage = "./404Error/index.html";
-  //   checkValidity() ? window.location.assign(nextPage) : " ";
-  // }
